1612af40c2d0ec64c6721d1f45ffa0db74859dbf
iancasyana16 <iancasyana@gmail.com>
2024-04-16 14:26:45 +0700

    test coding

---
+++ aplikasi_web/resources/views/component/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('component.header')
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+
+    @yield('content')
+
+    @include('component.script')
+
+</body>
+
+<footer>
+
+</footer>
+
+</html>
